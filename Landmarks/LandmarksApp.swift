5c78e82c29e42011955db969e6901bea97a6c7ca
Doug Wright <mrdougwright@gmail.com>
2021-03-17 23:41:09 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Doug Wright on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
