550d28a1446c820fa05be4a2fcb7974998c5d7af
Doug Wright <mrdougwright@gmail.com>
2021-03-20 12:02:53 -0500

    more lists & navigation

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,14 +9,22 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(
+                    destination: LandmarkDetail(landmark: landmark)){
+                    LandmarkRow(landmark: landmark)}
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone 12 mini", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+        }
     }
 }
